507b9d2df4703337a79179ffcb5bcdf856755de8
Kasovich Pavel <kasovich@fptrade.net>
2026-05-18 00:32:24 +0300

    view fix

--- src/components/internshipPrograms/InternshipProgramsList.js
+++ src/components/internshipPrograms/InternshipProgramsList.js
@@ -287,10 +287,10 @@ const InternshipProgramsList = ({ onEdit, onView }) => {
 
       <Grid container spacing={3}>
         {paginatedPrograms.map((program) => (
-          <Grid item xs={12} sm={6} md={4} key={program.id}>
+          <Grid key={program.id} size={{ xs: 12, sm: 6, md: 4 }}>
             <Card
               sx={{
-                width: '100%',
+                height: '100%',
                 display: 'flex',
                 flexDirection: 'column',
               }}
